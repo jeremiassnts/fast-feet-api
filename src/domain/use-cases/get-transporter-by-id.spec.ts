e22375d334cd8ba9a6ca225204263b7a9a4c59e7
Jeremias Santos <jeremias.santos@newcheck.com.br>
2025-01-05 22:53:00 -0300

    feat: use case and test for get transporter

---
+++ src/domain/use-cases/get-transporter-by-id.spec.ts
@@ -0,0 +1,27 @@
+import { InMemoryUsersRepository } from "test/repositories/in-memory-users-repository"
+import { UserFactory } from "test/factories/make-user"
+import { UserRoles } from "../entities/user"
+import { GetTransporterUseCase } from "./get-transporter-by-id"
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: GetTransporterUseCase
+
+describe('Get transporter by id', () => {
+    beforeEach(() => {
+        inMemoryUsersRepository = new InMemoryUsersRepository()
+        sut = new GetTransporterUseCase(inMemoryUsersRepository)
+    })
+
+    it('should be able to get transporter by id', async () => {
+        const transporter = UserFactory.makeUser({
+            role: UserRoles.TRANSPORTER
+        })
+        inMemoryUsersRepository.items.push(transporter)
+
+        const result = await sut.execute({ id: transporter.id })
+
+        expect(result.transporter).toEqual(expect.objectContaining({
+            id: transporter.id
+        }))
+    })
+})
