a0c094a67620c04b507c45581011723497dc1586
Jeremias Santos <jeremias.santos@newcheck.com.br>
2025-01-07 08:01:14 -0300

    feat: get order by id test and use case

---
+++ src/domain/use-cases/get-order-by-id.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { OrdersRepository } from "../repositories/orders-repository";
+
+interface GetOrderUseCaseRequest {
+    id: string
+}
+
+@Injectable()
+export class GetOrderUseCase {
+    constructor(private ordersRepository: OrdersRepository) { }
+    async execute({ id }: GetOrderUseCaseRequest) {
+        const order = await this.ordersRepository.findById(id)
+        return { order }
+    }
+}
